5b9d2467139e62292e0c2281d96add9868ff9b3f
magnuzoid <magnus.nielsen@gmail.com>
2017-01-22 19:27:35 +0100

    add username to user model

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,15 @@
+class RegistrationsController < Devise::RegistrationsController
+
+  def update
+    byebug
+  end
+
+  private
+  def sign_up_params
+    params.require(:user).permit(:username, :email, :password, :password_confirmation)
+  end
+
+  def account_update_params
+    params.require(:user).permit( :email, :password, :password_confirmation, :current_password)
+  end
+end
